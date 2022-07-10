3e18841bfddfc9be5bc1c9380e11ff8fa191e0d8
RememberTAE <2vlgofk@gmail.com>
2022-07-10 20:44:56 +0900

    Changed Camera Distance and Commit Test

--- Source/DodgeBall/DodgeBallCharacter.cpp
+++ Source/DodgeBall/DodgeBallCharacter.cpp
@@ -39,7 +39,7 @@ ADodgeBallCharacter::ADodgeBallCharacter()
 	// Create a camera boom (pulls in towards the player if there is a collision)
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(RootComponent);
-	CameraBoom->TargetArmLength = 400.0f; // The camera follows at this distance behind the character	
+	CameraBoom->TargetArmLength = 900.0f; // The camera follows at this distance behind the character	
 	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
 
 	// Create a follow camera
